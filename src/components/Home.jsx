2153bf9c2851043435f61667be880e57d5b29e9d
unknown <akitikorisegun2031@gmail.com>
2025-04-26 01:16:04 +0100

    initializing coinrise

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import Navbar from "./Navbar";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
   return (
     <>
       <Navbar />
-      <div>Home</div>
+      <div
+        className=" text-center font-semibold font-sans"
+        onClick={() => navigate("/dashboard")}
+      >
+        Goto Dashboard
+      </div>
     </>
   );
 };
