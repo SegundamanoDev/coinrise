3208b9d27e53b86ec55e49da3cd0cb3dc1ce8db1
unknown <akitikorisegun2031@gmail.com>
2025-05-10 23:39:01 +0100

    added scroll to top

--- src/components/AuthFormWrapper.jsx
+++ src/components/AuthFormWrapper.jsx
@@ -4,9 +4,7 @@ const AuthFormWrapper = ({ title, children }) => {
   return (
     <div className="min-h-screen bg-[#000000] flex items-center justify-center px-4">
       <div className="w-full max-w-md p-8 rounded-2xl shadow-xl my-3">
-        <h2 className="text-3xl font-bold text-center text-yellow-400 mb-6">
-          {title}
-        </h2>
+        <h2 className="text-3xl font-bold text-center mb-6">{title}</h2>
         {children}
       </div>
     </div>
